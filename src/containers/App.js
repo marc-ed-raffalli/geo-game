621807e798ff860c1bbbee08098f206e96a88b02
Marc-Ed Raffalli <raffalli.marc.ed@hotmail.fr>
2017-10-25 20:20:22 +0100

    Added deployment to gh-pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Redirect, Route} from 'react-router-dom';
+import {HashRouter as Router, Redirect, Route} from 'react-router-dom';
 
 import StartScreen from './StartScreen';
 import GameScreen from './GameScreen';
