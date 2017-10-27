47b3557e5a31db337ab21eba43ba4171ae2595a3
Marc-Ed Raffalli <raffalli.marc.ed@hotmail.fr>
2017-10-27 23:30:53 +0100

    Added link for project info / credits

--- src/components/page-footer/PageFooter.js
+++ src/components/page-footer/PageFooter.js
@@ -6,7 +6,7 @@ export default props => (
   <footer className="pb-2 d-flex justify-content-center flex-wrap no-gutters">
     <div className="col-12 col-md-auto pb-1 pb-md-0"><SocialNetworks locale={props.locale}/></div>
     <div className="col-12 col-md-auto offset-md-1 small align-self-center text-center">
-      <span>© 2017 - Marc Ed. Raffalli - </span><a href="./credits">Credits</a>
+      <span>© 2017 - Marc Ed. Raffalli - </span><a href="https://github.com/marc-ed-raffalli/geo-game">Info - Credits</a>
     </div>
   </footer>
 );
